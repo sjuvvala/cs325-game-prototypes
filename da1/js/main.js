181a20e9e6d1add3991e308d5766448d5d9a04b2
sjuvvala <60827099+sjuvvala@users.noreply.github.com>
2020-02-12 20:49:50 -0500

    Update main.js

--- da1/js/main.js
+++ da1/js/main.js
@@ -41,9 +41,9 @@ window.onload = function() {
         
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        //var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+        //var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        //text.anchor.setTo( 0.5, 0.0 );
         
         //game.physics.enable(ball, Phaser.Physics.ARCADE);
         
