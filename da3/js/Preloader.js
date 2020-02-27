5126a0e31c020594e8c4eebef91daffe68f983a0
sjuvvala <60827099+sjuvvala@users.noreply.github.com>
2020-02-26 23:31:19 -0500

    Update Preloader.js

--- da3/js/Preloader.js
+++ da3/js/Preloader.js
@@ -27,7 +27,7 @@ GameStates.makePreloader = function( game ) {
 	    //game.load.image('playButton', 'assets/start_button_ac.png');
             //game.load.atlas('playButton', 'assets/play_button.png', 'assets/play_button.json');
 	    game.load.atlas('playButton', 'assets/start_button_ac.png', 'assets/start_button_ac.json');
-            game.load.audio('titleMusic', ['assets/Poppers and Prosecco.mp3']);
+            //game.load.audio('titleMusic', ['assets/Poppers and Prosecco.mp3']);
             //	+ lots of other required assets here
             game.load.image( 'logo', 'assets/phaser.png' );
         },
@@ -50,11 +50,11 @@ GameStates.makePreloader = function( game ) {
             //	If you don't have any music in your game then put the game.state.start line into the create function and delete
             //	the update function completely.
             
-            if (game.cache.isSoundDecoded('titleMusic') && ready == false)
+            /*if (game.cache.isSoundDecoded('titleMusic') && ready == false)
             {
                 ready = true;
                 game.state.start('MainMenu');
-            }
+            }*/
     
         }
     
